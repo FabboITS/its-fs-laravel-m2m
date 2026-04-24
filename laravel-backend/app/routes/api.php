8a3635ab038d9d2ff97c838c04b1d8313c1465b7
Massimiliano <m.fabbo@itsprodigi.it>
2026-04-24 16:09:42 +0200

    Modified routes api.php

--- laravel-backend/app/routes/api.php
+++ laravel-backend/app/routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\CustomersController;
+use App\Http\Controllers\OrdersController;
+use App\Http\Controllers\ProductsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::apiResource('customers', CustomersController::class);
+Route::apiResource('products', ProductsController::class);
+Route::apiResource('orders', OrdersController::class);
+
+Route::get('customers/{customer}/orders', [CustomersController::class, 'orders']);
+Route::get('orders/{order}/products', [OrdersController::class, 'products']);
+Route::post('orders/{order}/products/{product}', [OrdersController::class, 'attachProduct']);
+Route::delete('orders/{order}/products/{product}', [OrdersController::class, 'detachProduct']);
+Route::get('products/{product}/orders', [ProductsController::class, 'orders']);
